9c8d48d2b6ee7e871b747fcfc77ce1a1a5af3295
Chris Rice <cd.rice23@gmail.com>
2023-03-02 09:38:50 -0600

    Quick fix for index page.

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,25 @@ import whiskyDbService from "../services/whiskyDbService";
 import surveyService from "../services/surveyService";
 import axios from "axios";
 
-export default function Home({ whiskies, surveyResults }) {
+export async function getStaticProps() {
+  const client = await clientPromise;
+  const db = client.db("scotch_tour_v2");
+  const whiskies = await db.collection("whisky_db").find({}).toArray();
+  if (!whiskies) {
+    return {
+      notFound: true,
+    };
+  }
+  return { props: { whiskies: JSON.parse(JSON.stringify(whiskies)) } };
+}
+
+export default function Home({ whiskies }) {
   const [whiskySet, setWhiskySet] = useRecoilState(whiskyListState);
   const [surveyData, setSurveyData] = useRecoilState(surveyResultsState);
 
   useEffect(() => {
     if (!whiskySet.length > 0) {
-      fetch("/api/whiskies")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          setWhiskySet(data);
-        });
+      setWhiskySet(whiskies);
     }
     if (!surveyData.length > 0) {
       fetch("/api/surveys")
