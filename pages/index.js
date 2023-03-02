a2312a47da34ca6a83a391934be290efe7b4e006
Chris Rice <cd.rice23@gmail.com>
2023-03-02 09:30:12 -0600

    Updated index page to remove serverless functions in favor of useEffect.

--- pages/index.js
+++ pages/index.js
@@ -9,49 +9,29 @@ import clientPromise from "../mongodb";
 import whiskyDbService from "../services/whiskyDbService";
 import surveyService from "../services/surveyService";
 import axios from "axios";
-import { ConstructionOutlined } from "@mui/icons-material";
-
-export async function getServerSideProps() {
-  const client = await clientPromise;
-  const db = client.db("scotch_tour_v2");
-  const whiskies = await db.collection("whisky_db").find({}).toArray();
-  const surveyResults = await db
-    .collection("visitor_survey")
-    .find({})
-    .toArray();
-
-  if (!whiskies) {
-    return {
-      notFound: true,
-    };
-  }
-  if (!surveyResults) {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      whiskies: JSON.parse(JSON.stringify(whiskies)),
-      surveyResults: JSON.parse(JSON.stringify(surveyResults)),
-    },
-  };
-}
 
 export default function Home({ whiskies, surveyResults }) {
   const [whiskySet, setWhiskySet] = useRecoilState(whiskyListState);
   const [surveyData, setSurveyData] = useRecoilState(surveyResultsState);
-  console.log(whiskySet);
-  console.log(surveyData);
-  console.log(whiskies);
-  console.log(surveyResults);
 
   useEffect(() => {
     if (!whiskySet.length > 0) {
-      setWhiskySet(whiskies);
+      fetch("/api/whiskies")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setWhiskySet(data);
+        });
     }
     if (!surveyData.length > 0) {
-      setSurveyData(surveyResults);
+      fetch("/api/surveys")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setSurveyData(data);
+        });
     }
   }, []);
   return (
